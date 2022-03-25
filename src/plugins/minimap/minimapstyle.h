5005ea0afb20114354f9e668b1ce3bd463eb584d
Miezhiko <Miezhiko@gmail.com>
2022-03-25 13:56:09 +0400

    minimap: fix compiling with clang
    Signed-off-by: Miezhiko <Miezhiko@gmail.com>
    Change-Id: I91891d2ab1edb114a27ebccd127fb80c49274966

--- src/plugins/minimap/minimapstyle.h
+++ src/plugins/minimap/minimapstyle.h
@@ -24,49 +24,49 @@
 
 namespace TextEditor
 {
-class BaseTextEditor;
+  class BaseTextEditor;
 }
 
 namespace Minimap
 {
 namespace Internal
 {
-class MinimapStyleObject;
+  class MinimapStyleObject;
 
-class MinimapStyle : public QProxyStyle
-{
-   Q_OBJECT
-public:
-   MinimapStyle(QStyle* style);
+  class MinimapStyle : public QProxyStyle
+  {
+     Q_OBJECT
+  public:
+     MinimapStyle(QStyle* style);
 
-   void drawComplexControl(ComplexControl control,
-                           const QStyleOptionComplex* option,
-                           QPainter* painter,
-                           const QWidget* widget = Q_NULLPTR) const override;
+     void drawComplexControl(ComplexControl control,
+                             const QStyleOptionComplex* option,
+                             QPainter* painter,
+                             const QWidget* widget = Q_NULLPTR) const override;
 
-   // need this due to QTBUG-24279
-   SubControl hitTestComplexControl(
-      ComplexControl control,
-      const QStyleOptionComplex* option,
-      const QPoint& pos,
-      const QWidget* widget = Q_NULLPTR) const override;
+     // need this due to QTBUG-24279
+     SubControl hitTestComplexControl(
+        ComplexControl control,
+        const QStyleOptionComplex* option,
+        const QPoint& pos,
+        const QWidget* widget = Q_NULLPTR) const override;
 
-   int pixelMetric(PixelMetric metric,
-                   const QStyleOption* option = Q_NULLPTR,
-                   const QWidget* widget = Q_NULLPTR) const override;
+     int pixelMetric(PixelMetric metric,
+                     const QStyleOption* option = Q_NULLPTR,
+                     const QWidget* widget = Q_NULLPTR) const override;
 
-   QRect subControlRect(ComplexControl cc,
-                        const QStyleOptionComplex* opt,
-                        SubControl sc,
-                        const QWidget* widget) const override;
+     QRect subControlRect(ComplexControl cc,
+                          const QStyleOptionComplex* opt,
+                          SubControl sc,
+                          const QWidget* widget) const override;
 
-   static QObject* createMinimapStyleObject(TextEditor::BaseTextEditor* editor);
+     static QObject* createMinimapStyleObject(TextEditor::BaseTextEditor* editor);
 
-private:
-   bool drawMinimap(const QStyleOptionComplex*,
-                    QPainter*,
-                    const QWidget*,
-                    MinimapStyleObject*) const;
-};
+  private:
+     bool drawMinimap(const QStyleOptionComplex*,
+                      QPainter*,
+                      const QWidget*,
+                      MinimapStyleObject*) const;
+  };
 }
 }
